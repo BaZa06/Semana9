9ee25ffbe2a2ab906cf8adfc44fb6b1820d6ff92
BaZa06 <bazeledon@uamv.edu.ni>
2026-05-15 16:12:20 -0600

    feat: configurar proyecto.

---
+++ src/main/java/ni/edu/uam/LinkedList/Ejemplo1.java
@@ -0,0 +1,15 @@
+package ni.edu.uam.LinkedList;
+
+import java.util.LinkedList;
+
+public class Ejemplo1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> numeros = new LinkedList<>();
+
+        numeros.add(10);
+        numeros.add(20);
+        numeros.add(30);
+
+        System.out.println(numeros);
+    }
+}
---
+++ src/main/java/ni/edu/uam/Ordenacion/Ejemplo1.java
@@ -0,0 +1,17 @@
+package ni.edu.uam.Ordenacion;
+import java.util.ArrayList;
+
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> numeros = new LinkedList<>();
+
+        numeros.add(10);
+        numeros.add(20);
+        numeros.add(30);
+
+        System.out.println(numeros);
+    }
+}
+
+
