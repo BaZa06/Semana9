7288efcdf74e1fcffe2ce90a4a5338d474aa5ac4
BaZa06 <bazeledon@uamv.edu.ni>
2026-05-15 16:42:23 -0600

    feat: Subiendo ejemplo 3 de ordenacion.

--- src/main/java/ni/edu/uam/Ordenacion/Ejemplo3.java
+++ src/main/java/ni/edu/uam/Ordenacion/Ejemplo3.java
@@ -1,4 +1,24 @@
 package ni.edu.uam.Ordenacion;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Ejemplo3 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(14);
+        numeros.add(7);
+        numeros.add(90);
+        numeros.add(25);
+
+        Collections.sort(
+                numeros,
+                Collections.reverseOrder()
+        );
+
+        System.out.println(numeros);
+
+    }
 }
