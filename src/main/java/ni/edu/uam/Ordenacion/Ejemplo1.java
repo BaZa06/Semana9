23fdd9a7e2bebbb73cea50123438ff7b77d16dc6
BaZa06 <bazeledon@uamv.edu.ni>
2026-05-15 16:36:24 -0600

    feat: Subiendo ejemplo 1 de ordenacion.

--- src/main/java/ni/edu/uam/Ordenacion/Ejemplo1.java
+++ src/main/java/ni/edu/uam/Ordenacion/Ejemplo1.java
@@ -1,16 +1,22 @@
 package ni.edu.uam.Ordenacion;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ejemplo1 {
-
     public static void main(String[] args) {
-        LinkedList<Integer> numeros = new LinkedList<>();
 
-        numeros.add(10);
-        numeros.add(20);
-        numeros.add(30);
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(80);
+        numeros.add(25);
+        numeros.add(15);
+        numeros.add(60);
+        numeros.add(5);
+
+        Collections.sort(numeros);
 
         System.out.println(numeros);
+
     }
 }
 
