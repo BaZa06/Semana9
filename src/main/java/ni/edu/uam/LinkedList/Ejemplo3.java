f1d62b1ed0beaee8b9230ddad4d252ca3ad29954
BaZa06 <bazeledon@uamv.edu.ni>
2026-05-15 16:31:04 -0600

    feat: subiendo archivos.

--- src/main/java/ni/edu/uam/LinkedList/Ejemplo3.java
+++ src/main/java/ni/edu/uam/LinkedList/Ejemplo3.java
@@ -1,4 +1,19 @@
 package ni.edu.uam.LinkedList;
 
+import java.util.LinkedList;
+
 public class Ejemplo3 {
+    public static void main(String[] args) {
+        LinkedList<String> productos = new LinkedList<>();
+
+        productos.add("Laptop");
+        productos.add("Mouse");
+        productos.add("Teclado");
+
+        productos.remove("Mouse");
+
+        System.out.println(productos);
+
+    }
+    }
 }
